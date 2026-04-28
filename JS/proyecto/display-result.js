0b8cfbd7097b95de262ab1b15eb75a12c72e4083
masakk1 <masakk1@proton.me>
2026-04-28 14:49:26 +0200

    feat(U21): Store Artist info in object

--- JS/proyecto/display-result.js
+++ JS/proyecto/display-result.js
@@ -1,1 +1,9 @@
 "use strict";
+
+class Artist {
+  constructor(img, name, tags) {
+    this.img = img;
+    this.name = name;
+    this.tags = tags;
+  }
+}
--- JS/proyecto/search.js
+++ JS/proyecto/search.js
@@ -4,5 +4,17 @@ function search_artist(query) {
   return HTTP.ajax(
     "GET",
     `http://musicbrainz.org/ws/2/artist?query=${query}`,
-  ).then((d) => d.artists);
+  ).then((data) => {
+    let artists = [];
+
+    data.artists.forEach((a, i) => {
+      const tags = a.tags
+        ?.filter((t) => t.count > 0)
+        .toSorted((t1, t2) => t2.count - t1.count)
+        .map((t) => t.name);
+      artists[i] = new Artist("./assets/64x64.svg", a.name, tags);
+    });
+
+    return artists;
+  });
 }
