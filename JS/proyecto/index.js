f52f5b19a506db5fb344193f791ac8b293dc1ab8
masakk1 <masakk1@proton.me>
2026-04-29 22:13:01 +0200

    feat!(api): Change search to form, and hook to search function

--- JS/proyecto/index.js
+++ JS/proyecto/index.js
@@ -1,7 +1,23 @@
 const resultContainer = document.getElementById("result-container");
 
-HTTP.search_artist("Dr.%20Dre").then((artists) => {
-  artists.forEach((a) => {
-    resultContainer.appendChild(a.draw());
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+
+function cleanContainer() {
+  while (resultContainer.firstChild) {
+    resultContainer.removeChild(resultContainer.lastChild);
+  }
+}
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevents the page from reloading
+
+  // Call your search function here
+  HTTP.search_artist(searchInput.value).then((artists) => {
+    cleanContainer();
+
+    artists.forEach((a) => {
+      resultContainer.appendChild(a.draw());
+    });
   });
 });
